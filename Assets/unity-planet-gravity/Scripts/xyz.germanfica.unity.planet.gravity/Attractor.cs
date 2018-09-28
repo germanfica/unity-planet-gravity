8b16e46195e61ce0aa8043dfd82a21faab3f0df4
German Fica <germafica@gmail.com>
2018-09-28 03:02:53 +0100

    Add assets to the project
    The materials are brown and green color.
    
    The scene contains a planet and player example.
    
    The included scripts are raycast, gravity and player controller.

---
+++ Assets/unity-planet-gravity/Scripts/xyz.germanfica.unity.planet.gravity/Attractor.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace xyz.germanfica.unity.planet.gravity
+{
+    public class Attractor : MonoBehaviour
+    {
+        public static List<Attractor> Attractors;
+        public float gravity = -10;
+        private Transform m_Transform;
+        private Rigidbody m_Rigidbody;
+
+        /* Apply gravity to the game object
+         */
+        public void Attract(Transform body)
+        {
+            Vector3 gravityUp = (body.position - transform.position).normalized;
+            Vector3 bodyUp = body.up;
+            body.GetComponent<Rigidbody>().AddForce(gravityUp * gravity);
+            Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, gravityUp) * body.rotation;
+
+            body.rotation = Quaternion.Slerp(body.rotation, targetRotation, 50 * Time.deltaTime);
+        }
+
+        /* All necessary variables to use gravity are initialized 
+         */
+        void Start()
+        {
+            m_Rigidbody = GetComponent<Rigidbody>();
+            m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+            m_Rigidbody.useGravity = false;
+
+            m_Transform = GetComponent<Transform>();
+        }
+
+        /* Applies gravity to game objects associated with the script
+         * 
+         * Note: each game object can have its own gravity
+         */
+        void FixedUpdate()
+        {
+            if (m_Rigidbody != null && m_Transform != null)
+            {
+                foreach (Attractor attractor in Attractors)
+                {
+                    if (attractor != this)
+                        attractor.Attract(m_Transform);
+                }
+            }
+        }
+
+        /* Add this game object to the list
+         */
+        void OnEnable()
+        {
+            if (Attractors == null)
+                Attractors = new List<Attractor>();
+
+            Attractors.Add(this);
+        }
+
+        /* When disabled, it must be deleted from the list
+         */
+        void OnDisable()
+        {
+            Attractors.Remove(this);
+        }
+    }
+}
---
+++ Assets/unity-planet-gravity/Scripts/xyz.germanfica.unity.planet.gravity/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace xyz.germanfica.unity.planet.gravity
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public Rigidbody rig;
+        RaycastHit hit;
+
+        void Start()
+        {
+            rig.useGravity = false; // Disables Gravity
+            rig.constraints = RigidbodyConstraints.FreezeRotation;
+        }
+
+        void Update()
+        {
+            if (Physics.Raycast(transform.position, -transform.up, out hit))
+            {
+                Debug.DrawLine(transform.position, hit.point, Color.cyan);
+            }
+        }
+
+        void FixedUpdate()
+        {
+            Move();
+        }
+
+        /* Character movement
+         */
+        private void Move()
+        {
+            var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+            var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+            transform.Rotate(0, x, 0);
+            transform.Translate(0, 0, z);
+        }
+    }
+}

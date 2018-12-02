67aeff1fc8e3ebc5c0fa07ab2f3a45f1f698456c
German Fica <germafica@gmail.com>
2018-12-02 13:08:31 -0300

    Basic jump action
    The character now can jump from any location on the planet

--- Assets/unity-planet-gravity/Scripts/xyz.germanfica.unity.planet.gravity/PlayerController.cs
+++ Assets/unity-planet-gravity/Scripts/xyz.germanfica.unity.planet.gravity/PlayerController.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 namespace xyz.germanfica.unity.planet.gravity
@@ -7,24 +7,65 @@ namespace xyz.germanfica.unity.planet.gravity
     {
         public Rigidbody rig;
         RaycastHit hit;
+        public bool freezeRotation = true;
+
+        public int forceConst = 4;
+        private bool canJump;
 
         void Start()
         {
-            rig.useGravity = false; // Disables Gravity
-            rig.constraints = RigidbodyConstraints.FreezeRotation;
+            Ini();
         }
 
         void Update()
         {
+            // Raycast (doesn't affect gameplay)
             if (Physics.Raycast(transform.position, -transform.up, out hit))
             {
                 Debug.DrawLine(transform.position, hit.point, Color.cyan);
+            }
+            // Jump Action
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                canJump = true;
             }
         }
 
         void FixedUpdate()
         {
             Move();
+            Jump();
+        }
+
+        /* Some initializations
+         */
+        private void Ini()
+        {
+            rig.useGravity = false; // Disables Gravity
+            if (freezeRotation)
+            {
+                rig.constraints = RigidbodyConstraints.FreezeRotation;
+            }
+            else
+            {
+                rig.constraints = RigidbodyConstraints.None;
+            }
+        }
+
+        /* Character jump
+         */
+        private void Jump()
+        {
+            if (canJump)
+            {
+                canJump = false;
+                // AddForce (useless)
+                //rig.AddForce(0, forceConst, 0, ForceMode.Impulse);
+                // AddForceAtPosition (useless too)
+                //rig.AddForceAtPosition(new Vector3(0, 0, forceConst), rig.transform.position, ForceMode.Impulse);
+                // AddRelativeForce (successful)
+                rig.AddRelativeForce(0, forceConst, 0, ForceMode.Impulse);
+            }
         }
 
         /* Character movement
